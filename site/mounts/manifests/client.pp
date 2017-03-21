d5508cec0d43828ac84c39fe26acfac3a5bfd860
suckatrash <suckatrash@users.noreply.github.com>
2017-03-21 16:37:49 -0700

    this broke again and I don't know why

--- site/mounts/manifests/client.pp
+++ site/mounts/manifests/client.pp
@@ -2,7 +2,7 @@ class mounts::client {
   class { 'nfs::client':
     nfs_v4              => true,
     nfs_v4_idmap_domain => $::domain,
-    nfs_v4_mount_root   => '/srv',
+    nfs_v4_mount_root   => '/data',
   }
 
   Nfs::Client::Mount <<| |>>

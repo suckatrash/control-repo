eb31881915c9298342490c3373077eb14fe28679
suckatrash <suckatrash@users.noreply.github.com>
2017-04-01 19:37:33 -0700

    add curl

--- site/packages/manifests/base.pp
+++ site/packages/manifests/base.pp
@@ -1,6 +1,6 @@
 class packages::base {
 
-$oneswewant = ['tree','expect','telnet','zsh','git','vim']
+$oneswewant = ['tree','expect','telnet','zsh','git','vim','curl']
 
 package {$oneswewant:
   ensure => installed,

aeb28157256f51beec797bffbe7d6258fd996073
suckatrash <suckatrash@users.noreply.github.com>
2017-04-17 14:29:47 -0700

    more BS

--- site/test/manifests/init.pp
+++ site/test/manifests/init.pp
@@ -1,15 +1,16 @@
 class test {
 
-  schedule { 'patch_pending':
+  schedule { 'patch_pending2':
     period => hourly,
     range => "5:00 - 6:50",
     repeat => 500,
-    loglevel => debug,
+    loglevel => verbose,
   }
 
   exec { 'sched run of patch_pending script':
     command => '/bin/echo "running script"',
-    schedule => 'patch_pending',
+    schedule => 'patch_pending2',
+    loglevel => verbose,
   }
 
 }

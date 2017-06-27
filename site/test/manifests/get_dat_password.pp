4d165edce1aa6c2c2de067461f7ce4a673f06a1d
suckatrash <erik.hansen@puppet.com>
2017-06-27 05:49:55 +0000

    ??

--- site/test/manifests/get_dat_password.pp
+++ site/test/manifests/get_dat_password.pp
@@ -2,32 +2,32 @@ class test::get_dat_password {
 
 user {'user1':
   ensure => present,
-  password => get_dat_password(user1,test),
+  password => get_dat_password('user1','test'),
 }
 
 user {'user2':
   ensure => present,
-  password => get_dat_password(user2,test),
+  password => get_dat_password('user2','test'),
 }
 
 user {'user3':
   ensure => present,
-  password => get_dat_password(user3,test),
+  password => get_dat_password('user3','test'),
 }
 
 user {'user4':
   ensure => present,
-  password => get_dat_password(user4,test),
+  password => get_dat_password('user4','test'),
 }
 
 user {'user5':
   ensure => present,
-  password => get_dat_password(user5,test),
+  password => get_dat_password('user5','test'),
 }
 
 user {'user6':
   ensure => present,
-  password => get_dat_password(user6,test),
+  password => get_dat_password('user6','test'),
 }
 
 }

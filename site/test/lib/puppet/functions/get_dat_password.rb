9a5719a733a5b59bc2e611c32e9f54bdf4bef6fb
suckatrash <erik.hansen@puppet.com>
2017-06-27 06:15:08 +0000

    another attempt

--- site/test/lib/puppet/functions/get_dat_password.rb
+++ site/test/lib/puppet/functions/get_dat_password.rb
@@ -1,7 +1,24 @@
 Puppet::Functions.create_function(':get_dat_password') do  
+  dispatch :getit do
+    param 'String', :username
+  end
   
-def call(scope, *args)
-    manipulated_args = [scope] + args
-    self.class.dispatcher.dispatch(self, scope, manipulated_args)
+  def getit(username)
+    case username
+      when "user1"
+        return 'user1pass'
+      when "user2"
+        return 'user2pass'
+      when "user3"
+        return 'user3pass'
+      when "user4"
+        return 'user4pass'
+      when "user5"
+        return 'user5pass'
+      when "user6"
+        return 'user6pass'
+      else
+        return "error"
+    end
   end
 end
--- site/test/manifests/get_dat_password.pp
+++ site/test/manifests/get_dat_password.pp
@@ -2,32 +2,32 @@ class test::get_dat_password {
 
 user {'user1':
   ensure => present,
-  password => get_dat_password('user1','test'),
+  password => get_dat_password('user1'),
 }
 
 user {'user2':
   ensure => present,
-  password => get_dat_password('user2','test'),
+  password => get_dat_password('user2'),
 }
 
 user {'user3':
   ensure => present,
-  password => get_dat_password('user3','test'),
+  password => get_dat_password('user3'),
 }
 
 user {'user4':
   ensure => present,
-  password => get_dat_password('user4','test'),
+  password => get_dat_password('user4'),
 }
 
 user {'user5':
   ensure => present,
-  password => get_dat_password('user5','test'),
+  password => get_dat_password('user5'),
 }
 
 user {'user6':
   ensure => present,
-  password => get_dat_password('user6','test'),
+  password => get_dat_password('user6'),
 }
 
 }

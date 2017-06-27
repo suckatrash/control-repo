ffbc483fe04e9f041a6e3e39e2d3fb30e2015dea
suckatrash <erik.hansen@puppet.com>
2017-06-27 05:19:38 +0000

    more files

---
+++ site/test/lib/puppet/functions/get_dat_password.rb
@@ -0,0 +1,11 @@
+Puppet::Functions.create_function('test:get_dat_password') do  
+  dispatch :deprecation_gen do
+    param 'Any', :scope
+    repeated_param 'Any', :args
+  end  
+
+  def call(scope, *args)
+    manipulated_args = [scope] + args
+    self.class.dispatcher.dispatch(self, scope, manipulated_args)
+  end
+end
---
+++ site/test/lib/puppet/parser/functions/get_dat_password.rb
@@ -0,0 +1,24 @@
+module Puppet::Parser::Functions
+  newfunction(:get_dat_password, :type => :rvalue) do |args|
+
+  case user
+    when "user1"
+      return 'user1pass'
+    when "user2"
+      return 'user2pass'
+    when "user3"
+      return 'user3pass'
+    when "user4"
+      return 'user4pass'
+    when "user5"
+      return 'user5pass'
+    when "user6"
+      return 'user6pass'
+    else
+      return "error"
+  end
+
+  end
+
+end
+

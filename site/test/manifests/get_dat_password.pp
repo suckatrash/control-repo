84f5fb483c062f27d58f1b71828313934241b45f
suckatrash <erik.hansen@puppet.com>
2017-06-27 06:21:50 +0000

    add pw_hash

--- site/test/manifests/get_dat_password.pp
+++ site/test/manifests/get_dat_password.pp
@@ -2,32 +2,32 @@ class test::get_dat_password {
 
 user {'user1':
   ensure => present,
-  password => get_dat_password('user1'),
+  password => pw_hash(get_dat_password('user1'),'SHA-256',salt),,
 }
 
 user {'user2':
   ensure => present,
-  password => get_dat_password('user2'),
+  password => pw_hash(get_dat_password('user2'),'SHA-256',salt),
 }
 
 user {'user3':
   ensure => present,
-  password => get_dat_password('user3'),
+  password => pw_hash(get_dat_password('user3'),'SHA-256',salt),
 }
 
 user {'user4':
   ensure => present,
-  password => get_dat_password('user4'),
+  password => pw_hash(get_dat_password('user4'),'SHA-256',salt),
 }
 
 user {'user5':
   ensure => present,
-  password => get_dat_password('user5'),
+  password => pw_hash(get_dat_password('user5'),'SHA-256',salt),
 }
 
 user {'user6':
   ensure => present,
-  password => get_dat_password('user6'),
+  password => pw_hash(get_dat_password('user6'),'SHA-256',salt),
 }
 
 }

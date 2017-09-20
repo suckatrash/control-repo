15194a09770ca9fc1f2c0850db02fa89e5a9e8b3
suckatrash <erik.hansen@puppet.com>
2017-09-20 16:42:51 +0000

    test

--- site/test/manifests/rundir.pp
+++ site/test/manifests/rundir.pp
@@ -1,6 +1,6 @@
 class test::rundir {
 
-  notify {"rundir: ${:rundir}":}
+  notify {"rundir: ${::settings::rundir}":}
   notify {"certname: ${::settings::certname}":}
   notify {"storeconfigs_backend: ${::settings::storeconfigs_backend}":}
 

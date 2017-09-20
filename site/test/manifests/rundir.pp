073b24b5ee76484e0e8e0b50b5d3eea6500bda25
suckatrash <erik.hansen@puppet.com>
2017-09-20 16:25:56 +0000

    change scope

--- site/test/manifests/rundir.pp
+++ site/test/manifests/rundir.pp
@@ -1,6 +1,6 @@
 class test::rundir {
 
-  notify {"rundir: ${::settings::rundir}":}
+  notify {"rundir: ${settings::rundir}":}
   notify {"certname: ${::settings::certname}":}
   notify {"storeconfigs_backend: ${::settings::storeconfigs_backend}":}
 

25f5a6e8bd45d71e8698a161de9c5a3fe732001a
suckatrash <suckatrash@users.noreply.github.com>
2017-10-05 08:50:16 -0700

    fix quotation

--- site/test/manifests/26999.pp
+++ site/test/manifests/26999.pp
@@ -4,7 +4,7 @@ java_ks { 'wls_identity:truststore_wls':
   ensure => latest, 
   certificate => "$/tmp/local/localhost.crt", 
   target => "/tmp/local/schufa_trust.jks", 
-  password => "password, 
+  password => "password", 
   trustcacerts => true, 
 }
 

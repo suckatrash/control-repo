01f096670580f2772da4c1b6678d6e3ed4055fe4
suckatrash <suckatrash@users.noreply.github.com>
2018-01-31 08:27:51 -0800

    test some shit

---
+++ site/profile/manifests/links.pp
@@ -0,0 +1,21 @@
+class profile::links {
+
+file { '/usr/local/bin/inq':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  replace => false,
+  mode => '0755',
+  content => "test",
+}
+
+file { '/usr/local/bin/mpio':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  replace => false,
+  mode => '0755',
+  content => "test",
+}
+
+}

ffd58a1530315ca72313c3a7a64e635b58e4a3ac
Erik Hansen <suckatrash@users.noreply.github.com>
2018-06-20 12:55:37 -0700

    Update t30217.pp

--- site/profile/manifests/t30217.pp
+++ site/profile/manifests/t30217.pp
@@ -2,6 +2,14 @@ class profile::t30217 {
 
   include puppet_enterprise::packages
   Package <| tag == 'pe-master-packages' |>
+  
+  puppet_enterprise::trapperkeeper::bootstrap_cfg { 'certificate-authority-service' :
+    container => 'puppetserver',
+    namespace => 'puppetlabs.services.ca.certificate-authority-disabled-service',
+    service   => 'certificate-authority-disabled-service',
+    require   => Package['pe-puppetserver'],
+    notify    => Service['pe-puppetserver'],
+  }
 
 #puppet_enterprise::trapperkeeper::pe_service { 'puppetserver' : }
 #  service {'pe-puppetserver':

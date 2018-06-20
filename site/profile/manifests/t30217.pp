0c889b01d1702653412f114950a35808bc73c269
Erik Hansen <suckatrash@users.noreply.github.com>
2018-06-20 12:40:31 -0700

    Update t30217.pp

--- site/profile/manifests/t30217.pp
+++ site/profile/manifests/t30217.pp
@@ -1,6 +1,6 @@
 class profile::t30217 {
 
-puppet_enterprise::trapperkeeper::pe_service { 'pe-puppetserver' : }
+puppet_enterprise::trapperkeeper::pe_service { 'puppetserver' : }
 #  service {'pe-puppetserver':
 #    ensure => running,
 #    enable => true,

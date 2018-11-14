46966562923289133b5c9414ca2990ff591fe90a
suckatrash <erik.hansen@puppet.com>
2018-11-14 12:54:31 -0800

    oops

---
+++ subenvironment/modules/test/manifests/midserver.pp
@@ -0,0 +1,13 @@
+class test::midserver {
+  
+  $agentdir = "C:\\test\\notarealfolder"
+  
+  file { "test":
+    ensure => file,
+    mode => '0664',
+    backup => false,
+    replace => true,
+    path => "${agentdir}/agent/test.xml",
+  }
+  
+}

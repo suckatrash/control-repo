f7d2a05c8de83595b84b80ba52a8a851844a19f0
Erik Hansen <suckatrash@users.noreply.github.com>
2018-11-21 10:13:35 -0800

    hell

--- site/test/manifests/postgres2.pp
+++ site/test/manifests/postgres2.pp
@@ -5,13 +5,13 @@ class test::postgres2 (
    $grafana_hostname,
 ){
 
-        puppet_enterprise::pg::cert_whitelist_entry { "Allow telegraf user to connect to pe-puppetdb as telegraf":
-            user                          => "telegraf",
-            database                      => "pe-puppetdb",
-            allowed_client_certname       => $grafana_hostname,
-            pg_ident_conf_path            => $pg_ident_conf_path,
-            ip_mask_allow_all_users_ssl   => $ip_mask_allow_all_users_ssl,
-            ipv6_mask_allow_all_users_ssl => $ipv6_mask_allow_all_users_ssl,
-        }
+  puppet_enterprise::pg::cert_whitelist_entry { "allow telegraf user to connect to pe-puppetdb as telegraf":
+    user                          => "telegraf",
+    database                      => "pe-puppetdb",
+    allowed_client_certname       => $grafana_hostname,
+    pg_ident_conf_path            => $pg_ident_conf_path,
+    ip_mask_allow_all_users_ssl   => $ip_mask_allow_all_users_ssl,
+    ipv6_mask_allow_all_users_ssl => $ipv6_mask_allow_all_users_ssl,
+  }
         
 }

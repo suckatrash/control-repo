f86e9c518e13bcb1e07dd89c01efb3061de29267
Erik Hansen <suckatrash@users.noreply.github.com>
2018-11-21 10:12:17 -0800

    commas

--- site/test/manifests/postgres2.pp
+++ site/test/manifests/postgres2.pp
@@ -1,8 +1,8 @@
 class test::postgres2 (
-   $pg_ident_conf_path            = "/opt/puppetlabs/server/data/postgres/9.6/data/pg_ident.conf
+   $pg_ident_conf_path            = "/opt/puppetlabs/server/data/postgres/9.6/data/pg_ident.conf,
    $ip_mask_allow_all_users_ssl   = '0.0.0.0/0',
    $ipv6_mask_allow_all_users_ssl = '::/0',
-   $grafana_hostname
+   $grafana_hostname,
 ){
 
         puppet_enterprise::pg::cert_whitelist_entry { "Allow telegraf user to connect to pe-puppetdb as telegraf":

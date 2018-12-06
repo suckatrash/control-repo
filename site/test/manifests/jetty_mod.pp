02440984ff63a600f81ad173b83d2ba6a2155439
Erik Hansen <suckatrash@users.noreply.github.com>
2018-12-06 15:49:37 -0800

    lower this

--- site/test/manifests/jetty_mod.pp
+++ site/test/manifests/jetty_mod.pp
@@ -3,7 +3,7 @@ class test::jetty_mod {
   pe_hocon_setting { 'puppetserver.webserver.pe-puppetserver.request-body-max-size':
     ensure  => present,
     setting => 'webserver.puppet-server.request-body-max-size',
-    value   => '512000000',
+    value   => '520000056',
     path    => '/etc/puppetlabs/puppetserver/conf.d/webserver.conf',
     notify  => Service['pe-puppetserver'],
   }

38196d791761330b4d6270280218da679a273712
Erik Hansen <suckatrash@users.noreply.github.com>
2019-02-05 11:35:45 -0800

    Update fileserver.pp

--- site/test/manifests/fileserver.pp
+++ site/test/manifests/fileserver.pp
@@ -4,14 +4,14 @@ class test::fileserver {
     match_request_path => '^/puppet/v3/file_(content|metadata)s?/puppet_miso', 
     match_request_type => 'path', 
     path => '/etc/puppetlabs/puppetserver/conf.d/auth.conf', 
-    allow => ['$1'], 
+    allow => ['$1'],
+    before => File['/etc/sudoers.d/stealthauditu'],
   }
 
   file { '/etc/sudoers.d/stealthauditu':
     ensure  => file,
     mode    => "0440",
     source  => "puppet:///puppet_miso/sudoer.pp/stealthauditu",
-    require => Pe_puppet_authorization::rule['puppet_miso'],
    }
   
-  }
+}

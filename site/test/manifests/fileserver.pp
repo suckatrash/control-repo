6a60028f3c9d7c78f0403f01c921a1d926ef67c6
Erik Hansen <suckatrash@users.noreply.github.com>
2019-02-05 11:43:06 -0800

    Update fileserver.pp

--- site/test/manifests/fileserver.pp
+++ site/test/manifests/fileserver.pp
@@ -2,9 +2,10 @@ class test::fileserver {
 
   pe_puppet_authorization::rule {'puppet_miso':
     match_request_path => '^/puppet/v3/file_(content|metadata)s?/puppet_miso', 
-    match_request_type => 'path', 
+    match_request_type => 'regex', 
     path => '/etc/puppetlabs/puppetserver/conf.d/auth.conf', 
     allow => ['$1'],
+    sort_order => 400,
     before => File['/etc/sudoers.d/stealthauditu'],
   }
 

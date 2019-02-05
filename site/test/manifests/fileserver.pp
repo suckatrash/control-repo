0ed939afbc662b0a2f7e620089b3a92ae6005967
Erik Hansen <suckatrash@users.noreply.github.com>
2019-02-05 11:39:29 -0800

    Update fileserver.pp

--- site/test/manifests/fileserver.pp
+++ site/test/manifests/fileserver.pp
@@ -1,6 +1,6 @@
 class test::fileserver {
 
-  pe_puppet_authorization::rule{'puppet_miso':
+  pe_puppet_authorization::rule {'puppet_miso':
     match_request_path => '^/puppet/v3/file_(content|metadata)s?/puppet_miso', 
     match_request_type => 'path', 
     path => '/etc/puppetlabs/puppetserver/conf.d/auth.conf', 

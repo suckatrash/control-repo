429dc6192a22978e94d5ee9e2091c46aca83c547
Erik Hansen <suckatrash@users.noreply.github.com>
2019-03-14 16:33:46 -0700

    Update migrate.sh

--- site/test/tasks/migrate.sh
+++ site/test/tasks/migrate.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-declare PT_NEW_MASTER
+declare PT_new_master
 
 PUPPET_CONFDIR='/etc/puppetlabs/puppet'
 PUPPET_BIN='/opt/puppetlabs/puppet/bin'
@@ -12,7 +12,7 @@ echo "Backing up existing certificates"
 tar czvf ${PUPPET_CONFDIR}sslbackup-$(date +%y%m%d%H%M%SZ).tar.gz ${PUPPET_CONFDIR}
 mv ${PUPPET_CONFDIR}/ssl /tmp
 
-echo "Setting noop and requesting new certificate from ${PT_NEW_MASTER}"
+echo "Setting noop and requesting new certificate from ${PT_new_master}"
 "${PUPPET_BIN}/puppet" config set noop=true
-"${PUPPET_BIN}/puppet" config set server=${PT_NEW_MASTER}
+"${PUPPET_BIN}/puppet" config set server=${PT_new_master}
 "${PUPPET_BIN}/puppet" agent -t

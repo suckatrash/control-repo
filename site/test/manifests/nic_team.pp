63d9c66ed74205c663fd5021703a3665599ed466
Erik Hansen <suckatrash@users.noreply.github.com>
2019-03-27 13:56:51 -0700

    Update nic_team.pp

--- site/test/manifests/nic_team.pp
+++ site/test/manifests/nic_team.pp
@@ -5,10 +5,22 @@ class test::nic_team {
     module        => 'NetworkingDsc',
     properties    => {
       Ensure                 => 'present',
-      Name                   => 'Team1 - VLAN 22',
+      Name                   => 'Team1 - HyperV',
       TeamingMode            => 'SwitchIndependent',
       LoadBalancingAlgorithm => 'HyperVPort',
       TeamMembers            => ['NIC1', 'NIC2'],
     }
-  }  
+  }
+  
+  dsc {'nic_team2:
+    resource_name => 'NetworkTeam',
+    module        => 'NetworkingDsc',
+    properties    => {
+      Ensure                 => 'present',
+      Name                   => 'Team2 - VLAN 22',
+      TeamingMode            => 'SwitchIndependent',
+      LoadBalancingAlgorithm => 'HyperVPort',
+      TeamMembers            => ['NIC3', 'NIC4'],
+    }
+  }
 }

15c21b5d691b135d7f83a7b949541cc36d454d25
Erik Hansen <suckatrash@users.noreply.github.com>
2019-03-27 15:02:17 -0700

    Update nic_team.pp

--- site/test/manifests/nic_team.pp
+++ site/test/manifests/nic_team.pp
@@ -2,8 +2,16 @@ class test::nic_team {
 
   dsc_networkteam{ 'team1':
     dsc_ensure                 => 'present',
-    dsc_name                   => 'team1',
-    dsc_teammembers            => ['nic1','nic2'],
+    dsc_name                   => 'Team1 - HyperV',
+    dsc_teammembers            => ['NIC1','NIC2'],
+    dsc_teamingmode            => 'SwitchIndependent',
+    dsc_loadbalancingalgorithm => 'Dynamic',
+  }
+  
+  dsc_networkteam{ 'team2':
+    dsc_ensure                 => 'present',
+    dsc_name                   => 'Team2 - VLAN 22',
+    dsc_teammembers            => ['NIC3','NIC4'],
     dsc_teamingmode            => 'SwitchIndependent',
     dsc_loadbalancingalgorithm => 'Dynamic',
   }

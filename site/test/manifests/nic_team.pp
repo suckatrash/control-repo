980dc1a2ee0260e9662a2ce50c64f19d8bb76559
Erik Hansen <suckatrash@users.noreply.github.com>
2019-03-27 14:57:46 -0700

    Update nic_team.pp

--- site/test/manifests/nic_team.pp
+++ site/test/manifests/nic_team.pp
@@ -1,34 +1,10 @@
 class test::nic_team {
 
-  include chocolatey
-
-  package { 'NetworkingDsc':
-    ensure   => latest,
-    provider => 'chocolatey',
-    before    => [Dsc['nic_team1'],Dsc['nic_team2']],
-  }
-
-  dsc {'nic_team1':
-    resource_name => 'NetworkTeam',
-    module        => 'NetworkingDsc',
-    properties    => {
-      ensure                 => 'present',
-      name                   => 'Team1 - HyperV',
-      teamingmode            => 'SwitchIndependent',
-      loadbalancingalgorithm => 'HyperVPort',
-      teammembers            => ['NIC1', 'NIC2'],
-    }
-  }
-  
-  dsc {'nic_team2':
-    resource_name => 'NetworkTeam',
-    module        => 'NetworkingDsc',
-    properties    => {
-      ensure                 => 'present',
-      name                   => 'Team2 - VLAN 22',
-      teamingmode            => 'SwitchIndependent',
-      loadbalancingalgorithm => 'HyperVPort',
-      teammembers            => ['NIC3', 'NIC4'],
-    }
+  dsc_networkteam{ 'team1':
+    dsc_ensure                 => 'present',
+    dsc_name                   => 'team1',
+    dsc_teammembers            => ['nic1','nic2'],
+    dsc_teamingmode            => 'SwitchIndependent',
+    dsc_loadbalancingalgorithm => 'Dynamic',
   }
 }

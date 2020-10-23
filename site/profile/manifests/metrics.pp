54c76c896290b8a58aa7a145ff5770c86ec4e8ef
Erik Hansen <suckatrash@users.noreply.github.com>
2020-10-23 15:29:54 -0700

    Update metrics.pp

--- site/profile/manifests/metrics.pp
+++ site/profile/manifests/metrics.pp
@@ -1,15 +1,5 @@
 class profile::metrics {
 
-  puppet_metrics_dashboard::profile::compiler{ $facts['networking']['fqdn']:
-    timeout => '5s',
-  }
-
-  puppet_metrics_dashboard::profile::puppetdb{ $facts['networking']['fqdn']:
-    timeout            => '5s',
-    enable_client_cert => false,
-    #puppetdb_metrics => lookup(puppet_metrics_dashboard::profile::puppetdb::puppetdb_metrics)
-  }
-
   include puppet_metrics_dashboard::profile::master::postgres_access
 
 }
